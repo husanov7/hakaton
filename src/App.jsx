662926820c4629b567141cf1334efe1e3457f79f
husanov7 <husanovshamsiddin7@gmail.com>
2024-07-25 13:19:31 +0300

    nav

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./style.scss";
+import BottomNav from "./components/navbar";
+export default function App() {
+  return (
+   <>
+    <div className="all">
+     
+      <BottomNav />
+    </div>
+   </>
+  );
+}
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,36 @@
+import "./style.scss";
+import React, { useState } from "react";
+import { FaHome, FaSearch, FaUserAlt } from "react-icons/fa";
+
+const BottomNav = () => {
+  const [active, setActive] = useState("home");
+
+  const handleNavClick = (nav) => {
+    setActive(nav);
+  };
+
+  return (
+    <div className="bottom-nav">
+      <div
+        className={`nav-item ${active === "home" ? "active" : ""}`}
+        onClick={() => handleNavClick("home")}
+      >
+        <FaHome />
+      </div>
+      <div
+        className={`nav-item ${active === "search" ? "active" : ""}`}
+        onClick={() => handleNavClick("search")}
+      >
+        <FaSearch />
+      </div>
+      <div
+        className={`nav-item ${active === "profile" ? "active" : ""}`}
+        onClick={() => handleNavClick("profile")}
+      >
+        <FaUserAlt />
+      </div>
+    </div>
+  );
+};
+
+export default BottomNav;
