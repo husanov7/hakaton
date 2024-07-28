31a15102aab062584274b83511ba2a5965865739
unknown <baluplay15@gmail.com>
2024-07-28 13:38:31 +0500

    not

--- src/pages1/index.jsx
+++ src/pages1/index.jsx
@@ -1,17 +1,21 @@
-
-
 import React, { useState } from "react";
-import { Button, Dialog, DialogBody, DialogFooter, DialogHeader } from "@material-tailwind/react";
+import {
+  Button,
+  Dialog,
+  DialogBody,
+  DialogFooter,
+  DialogHeader,
+} from "@material-tailwind/react";
 import api from "../axios"; // Correct the path if axios.js is in a different directory
 
 const days = [
-  { day: 'Пн', time: '8:00 - 23:00' },
-  { day: 'Вт', time: '10:00 - 22:00' },
-  { day: 'Ср', time: '11:00 - 21:00' },
-  { day: 'Чт', time: '11:00 - 23:00' },
-  { day: 'Пт', time: '10:00 - 23:00' },
-  { day: 'Сб', time: '9:00 - 20:00' },
-  { day: 'Вс', time: '12:00 - 00:00' },
+  { day: "Пн", time: "8:00 - 23:00" },
+  { day: "Вт", time: "10:00 - 22:00" },
+  { day: "Ср", time: "11:00 - 21:00" },
+  { day: "Чт", time: "11:00 - 23:00" },
+  { day: "Пт", time: "10:00 - 23:00" },
+  { day: "Сб", time: "9:00 - 20:00" },
+  { day: "Вс", time: "12:00 - 00:00" },
 ];
 
 export default function Onas() {
@@ -23,15 +27,18 @@ export default function Onas() {
 
   const newMessage = (e) => {
     e.preventDefault();
-    api.post("/message", { nomi: name, nomer: number })
-      .then(res => alert("bajarildi!"))
-      .catch(err => alert("Error: " + err.message));
-  }
+    api
+      .post("/message", { nomi: name, nomer: number })
+      .then((res) => alert("bajarildi!"))
+      .catch((err) => alert("Error: " + err.message));
+      setNumber('')
+      setName('')
+  };
 
   const handleOpen = () => setOpen(!open);
 
   return (
-    <div className="container w-[500px] m-[auto] md:[300px] "> 
+    <div className="container w-[500px] m-[auto] md:[300px] ">
       <div className="bg-white shadow-md rounded-lg p-6">
         <div className="flex flex-row justify-between">
           <div>
@@ -45,7 +52,11 @@ export default function Onas() {
             </p>
           </div>
           <div>
-            <img className="w-[200px]" src="http://giotto.uz/wp-content/uploads/2018/09/logo.png" alt="logo" />
+            <img
+              className="w-[200px]"
+              src="http://giotto.uz/wp-content/uploads/2018/09/logo.png"
+              alt="logo"
+            />
           </div>
         </div>
         <div className="flex flex-col items-center">
@@ -58,18 +69,30 @@ export default function Onas() {
               <button
                 key={dayItem.day}
                 onClick={() => setSelectedDay(dayItem)}
-                className={`py-2 px-4 rounded ${selectedDay.day === dayItem.day ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+                className={`py-2 px-4 rounded ${
+                  selectedDay.day === dayItem.day
+                    ? "bg-blue-500 text-white"
+                    : "bg-gray-200"
+                }`}
               >
                 {dayItem.day}
               </button>
             ))}
           </div>
           <div className="modal pt-[10px]">
-            <button className="w-[400px] bg-blue-500 text-[white] h-[45px] rounded-[20px]" onClick={handleOpen}>собшите нам</button>
+            <button
+              className="w-[400px] bg-blue-500 text-[white] h-[45px] rounded-[20px]"
+              onClick={handleOpen}
+            >
+              собшите нам
+            </button>
             <Dialog open={open} handler={handleOpen}>
               <DialogHeader>Massage</DialogHeader>
               <DialogBody className="gap-[20px] flex flex-col">
-                <form onSubmit={newMessage} className="flex flex-col gap-[10px]">
+                <form
+                  onSubmit={newMessage}
+                  className="flex flex-col gap-[10px]"
+                >
                   <div className="flex flex-row gap-[30px]">
                     <input
                       className="border-solid p-[5px] w-[100%] border-[1px] border-black rounded-[10px]"
@@ -97,7 +120,12 @@ export default function Onas() {
                 </form>
               </DialogBody>
               <DialogFooter>
-                <Button variant="text" color="red" onClick={handleOpen} className="mr-1">
+                <Button
+                  variant="text"
+                  color="red"
+                  onClick={handleOpen}
+                  className="mr-1"
+                >
                   <span>Cancel</span>
                 </Button>
                 <Button variant="gradient" color="blue" onClick={handleOpen}>
@@ -109,7 +137,9 @@ export default function Onas() {
         </div>
         <div className="pt-[20px]">
           <p className="text-gray-700 mb-4">Адрес: </p>
-          <p className="text-black font-semibold mb-4 text-[20px]">ул. Мукуми 200</p>
+          <p className="text-black font-semibold mb-4 text-[20px]">
+            ул. Мукуми 200
+          </p>
         </div>
         <div className="space-y-2 p-[5px]">
           <div className="flex flex-row justify-between">
@@ -122,7 +152,10 @@ export default function Onas() {
             <p>giottoilgelato@gmail.com</p>
           </div>
           <hr />
-          <a href="https://www.instagram.com/m1rkomilovv/?next=%2F" className="text-blue-500 p-[5px]">
+          <a
+            href="https://www.instagram.com/m1rkomilovv/?next=%2F"
+            className="text-blue-500 p-[5px]"
+          >
             Instagram
           </a>
         </div>
